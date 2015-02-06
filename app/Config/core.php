482b5ffd2b4fd770713a612bf7941cfeeb016886
dprabin <dprabin@yahoo.com>
2015-02-06 09:57:41 +0545

    changed cipher seed and security salt

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhG63b0qyJfIxfa2guVoUubWwvniY2G0FgaC9mU');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '76809303657453542491749683635');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
