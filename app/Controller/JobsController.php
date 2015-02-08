b468be4985e0a6244975f4ef9583a491777f7b8a
dprabin <dprabin@yahoo.com>
2015-02-08 20:52:04 +0545

    added options to the job find query

--- app/Controller/JobsController.php
+++ app/Controller/JobsController.php
@@ -6,6 +6,14 @@ class JobsController extends AppController{
 	 * Defualt Index method
 	 */
 	public function index(){
+		//Set Query options
+		$options = array(
+			'order' => array('Job.created' => 'asc'),
+			'limit' => 2
+			);
+		// Get Job Information
+		$jobs = $this->Job->find('all', $options);
+
 		$this->set('jobs',$jobs);
 	}
 
