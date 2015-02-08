2d4b5e71b4d10d80552de8332a501e739f249ff9
dprabin <dprabin@yahoo.com>
2015-02-08 20:31:59 +0545

    added default index method to JobsController

--- app/Controller/JobsController.php
+++ app/Controller/JobsController.php
@@ -1,4 +1,15 @@
 <?php
+class JobsController extends AppController{
+	public $name = 'Jobs';
 
+	/*
+	 * Defualt Index method
+	 */
+	public function index(){
+		$this->set('jobs',$jobs);
+
+	}
+
+}
 
 ?>
