3f8c78145a24178bef2b6db0302d56519f60b76c
dprabin <dprabin@yahoo.com>
2015-02-09 15:20:38 +0545

    added browse method

--- app/Controller/JobsController.php
+++ app/Controller/JobsController.php
@@ -16,6 +16,26 @@ class JobsController extends AppController{
 		$this->set('jobs',$jobs);
 	}
 
+	/*
+	 * Job Browse Method
+	*/
+	public function browse($category=null){
+		//Init Conditions array
+		$conditions = array();
+		
+		if($category != null){
+			$conditions[]=array('Job.category_id like'=>'%'.$category.'%');
+		}
+		//Set query options
+		$options = array(
+			'order'=>array('Job.created'=>'desc'),
+			'conditions'=>$conditions,
+			'limit'=>8 );
+
+		// Get Job Information
+		$jobs = $this->Job->find('all', $options);
+		$this->set('jobs',$jobs);
+	}
 }
 
 ?>
