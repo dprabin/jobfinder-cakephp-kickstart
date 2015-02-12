36049569c4339a51a52833a0558d6b2bd059db7f
dprabin <dprabin@yahoo.com>
2015-02-12 17:42:49 +0545

    define user role variable and pass to view

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -3,7 +3,10 @@ class UsersController extends AppController{
 	public $name = 'Users';
 
 	public function register(){
-		
+		//Define User Roles to be passed to view
+		//Better to be implemented through db table
+		$userRole = array('Job Seeker'=>'Job Seeker', 'Employer'=>'Employer');
+		$this->set('userRole'=>$userRole);
 	}
 
 }
