5ba9d1f454f7060a9fa26c4a77ca26f49507fcc5
dprabin <dprabin@yahoo.com>
2015-02-12 22:16:54 +0545

    added comments to define functions and validation

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,14 +1,33 @@
 <?php
+App::uses('AppModel','Model');
+App::uses('SimplePasswordHasher','Controller/Component/Auth');
+
 class User extends AppModel{
+	public $name = 'User';
+
+	/*
+	 * Function to hash password before saving it
+	 * This function is executed before any save action is executed
+	 * in users model
+	 */
+	public function beforeSave($options = array()) {
+		if(isset($this->data[$this->alias]['password'])){ //if data has password field set
+			$passwordHasher = new SimplePasswordHasher();
+			$this->data[$this->alias]['password'] = $passwordHasher->hash($this->data[$this->alias]['password']); //hash that password
+		}
+		return true;
+	}
+
+	//Validation rules for the input fields
 	public $validate = array(
 		'first_name'	=> array(
 			'alphaNumeric'	=> array(
-				'rule'		=> 'alphaNumeric',
-				'required'	=> TRUE,
-				'message'	=> 'Letters and Numbers only'),
-			'notEmpty'		=> array(
-				'rule'		=> 'notEmpty',
-				'message'	=> 'Please enter your First Name')
+				'rule'		=> 'alphaNumeric',				//firstname should alphanumeric
+				'required'	=> TRUE,						//Required field
+				'message'	=> 'Letters and Numbers only'),	//If not alphanumeric, display message letters and numbers only
+			'notEmpty'		=> array(						//Rules when field is empty
+				'rule'		=> 'notEmpty',					//If the field is empty
+				'message'	=> 'Please enter your First Name') //Display Please enter your first name
 			),
 		'last_name'	=> array(
 			'alphaNumeric'	=> array(
