500ceb1ca8f02e514a5dd66db7d49f448013337c
dprabin <dprabin@yahoo.com>
2015-02-12 22:17:47 +0545

    register new uwer

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1,20 +1,6 @@
 <?php
-App::uses('AppModel','Model');
-App::uses('SimplePasswordHasher','Controller/Component/Auth');
-
 class UsersController extends AppController{
 	public $name = 'Users';
-
-	/*
-	 * Function to hash password before saving it
-	 */
-	public function beforeSave($options = array()){
-		if(isset($this->data[$this->alias]['password'])){
-			$passwordHasher = new SimplePasswordHasher();
-			$this->data[$this->alias]['password'] = $passwordHasher->hash($this->data[$this->alias]['password']);
-		}
-		return true;
-	}
 
 	public function register(){
 		//Define User Roles to be passed to view
@@ -24,14 +10,15 @@ class UsersController extends AppController{
 
 		if($this->request->is('post')){
 			$this->User->create();
+			
 			if($this->User->save($this->request->data)){
 				$this->Session->setFlash(__('You are now registered and may login'));
 				return $this->redirect(array('controller'=>'jobs','action'=>'index'));
 			} else {
 				$this->Session->setFlash(__('Unable to create new user'));
+
 			}
 		}
 	}
-
 }
 ?>
